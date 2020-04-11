4b6967dedfd810f4918c04b2ad637ff9db1ad0b3
James Hulse <james.hulse@gmail.com>
2020-04-11 11:16:14 +0100

    Fix deaths data type

--- src/lib/data.js
+++ src/lib/data.js
@@ -5,7 +5,7 @@ import countryLookup from './countryLookup';
 
 const dataUrls = {
     'cases': 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv',
-    'death': 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
+    'deaths': 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv'
 };
 
 const dataCache = {};
